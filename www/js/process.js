cce873e97a0c609cd76bb58a0a131c82f146430a
oliver alipio <31386369+BlackyOliver@users.noreply.github.com>
2017-12-07 13:32:03 +0800

    Add files via upload

---
+++ www/js/process.js
@@ -0,0 +1,86 @@
+let process = {
+  menu:function(){
+    let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">RESPERATORY GALLERY</div>
+              </div>
+            </div>
+
+ <img src={"img/2.jpg"} width={"100%"}/>
+ <center><p>A nostril (or naris /ˈneɪrɪs/, plural nares /ˈneɪriːz/) is one of the two channels of the nose, from the point where they bifurcate to the external opening. In birds and mammals, they contain branched bones or cartilages called turbinates, whose function is to warm air on inhalation and remove moisture on exhalation. </p></center>
+<br />
+
+ <img src={"img/3.jpg"} width={"100%"}/>
+ <center><p> Paranasal sinuses are a group of four paired air-filled spaces that surround the nasal cavity. The maxillary sinuses are located under the eyes; the frontal sinuses are above the eyes; the ethmoidal sinuses are between the eyes and the sphenoidal sinuses are behind the eyes. </p></center>
+<br />
+
+ <img src={"img/4.jpg"} width={"100%"}/>
+ <center><p> The oral cavity (mouth) and oropharynx (throat) Theoral cavity includes the lips, the inside lining of the lips and cheeks (buccal mucosa), the teeth, the gums, the front two-thirds of the tongue, the floor of the mouth below the tongue, and the bony roof of the mouth (hard palate. </p></center>
+<br />
+
+ <img src={"img/5.jpg"} width={"100%"}/>
+ <center><p> The pharynx (plural: pharynges) is the part of the throat that is behind the mouth and nasal cavity and above the esophagus and the larynx, or the tubes going down to the stomach and the lungs. </p></center>
+<br />
+
+ <img src={"img/6.jpg"} width={"100%"}/>
+ <center><p> The epiglottis is a flap made of elastic cartilage covered with a mucous membrane, attached to the entrance of the larynx. It projects obliquely upwards behind the tongue and the hyoid bone, pointing dorsally. It stands open during breathing, allowing air into the larynx. </p></center>
+<br />
+
+ <img src={"img/7.jpg"} width={"100%"}/>
+ <center><p> The larynx, commonly called the voice box, is an organ in the top of the neck of tetrapods involved in breathing, producing sound, and protecting the trachea against food aspiration. </p></center>
+<br />
+
+ <img src={"img/8.jpg"} width={"100%"}/>
+ <center><p> The trachea, colloquially called the windpipe, is a cartilaginous tube that connects the pharynx and larynx to the lungs, allowing the passage of air, and so is present in almost all air-breathing animals with lungs. The trachea extends from the larynx and branches into the two primary bronchi. </p></center>
+<br />
+
+ <img src={"img/9.jpg"} width={"100%"}/>
+ <center><p>A bronchus, is a passage of airway in the respiratory tract that conducts air into the lungs. The first bronchito branch from the trachea are the right main bronchus and the left main bronchus. </p></center>
+<br />
+
+ <img src={"img/10.jpg"} width={"100%"}/>
+ <center><p> There are 10 bronchopulmonary segments in the right lung: three in the superior lobe, two in the middle lobe, and five in the inferior lobe. Some of the segments may fuse in the left lung to form usually 8-9 segments (4–5 in the upper lobe and 4–5 in the lower lobe). </p></center>
+<br />
+
+ <img src={"img/11.jpg"} width={"100%"}/>
+ <center><p>Right middle lobe syndrome (RMLS) generally refers to atelectasis in the right middle lobe of the lung. It is caused by various etiologies and has no consistent clinical definition </p></center>
+<br />
+
+ <img src={"img/12.jpg"} width={"100%"}/>
+ <center><p>A major organ of the endocrine system, the anteriorpituitary (also called the adenohypophysis or parsanterior), is the glandular, anterior lobe that together with the posterior lobe (posterior pituitary, or the neurohypophysis) makes up the pituitary gland (hypophysis). </p></center>
+<br />
+
+ <img src={"img/13.jpg"} width={"100%"}/>
+ <center><p>The diaphragm is a barrier method of birth control. It is moderately effective, with a one-year failure rate of around 12% with typical use. It is placed over the cervix with spermicide before sex and left in place for at least six hours after sex </p></center>
+<br />
+
+ <img src={"img/14.jpg"} width={"100%"}/>
+ <center><p>A bronchus, is a passage of airway in the respiratory tract that conducts air into the lungs. The first bronchito branch from the trachea are the right mainbronchus and the left main bronchus </p></center>
+<br />
+
+
+</div>
+</div>
+</div>
+</div>
+</div>;
+    ReactDOM.render(content,document.getElementById('root'));
+  },
+
+  loading:function(){
+    let content = <div>
+          LOADINGGG .... RESPIRATORY GALLERY .....
+    </div>;
+    ReactDOM.render(content,document.getElementById('root'));
+  }
+}
+
+process.loading();
+setTimeout(function(){
+  process.menu();
+},1500);
